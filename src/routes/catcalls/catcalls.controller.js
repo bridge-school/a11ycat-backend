62fc835a8103127aef58852913ed1b0ec81a3958
Jasper Walton <32462443+jaswalt@users.noreply.github.com>
2019-03-09 12:17:16 -0500

    Update catcalls.controller.js

--- src/routes/catcalls/catcalls.controller.js
+++ src/routes/catcalls/catcalls.controller.js
@@ -12,28 +12,3 @@ const catcallsController = (req, res) => {
 module.exports = {
   catallsController
 };
-
-
-
-// let citiesRef = db.collection('conferences');
-// let allCities = [];
-
-// citiesRef
-// 	.get()
-// 	.then(snapshot => {
-// 		snapshot.forEach(doc => {
-// 			allCities.push({
-// 				[doc.id]: doc.data()
-// 			});
-// 		});
-// 	})
-// 	.catch(err => {
-// 		console.log('Error getting documents', err);
-// 	});
-
-// const conferenceController = (req, res) => {
-// 	//connect to firebase and get snapshot of conferences collection
-// 	res.json({
-// 		data: allCities
-// 	});
-// };
