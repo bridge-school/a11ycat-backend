dfc799e9ab9e989c52d380c2dd590eb413c54080
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2019-03-14 19:54:22 -0400

    Added keys to response objects

--- src/routes/catcalls/catcalls.controller.js
+++ src/routes/catcalls/catcalls.controller.js
@@ -5,17 +5,12 @@ const catcallsController = (req, res) => {
     .get()
     .then(snapshot =>
       snapshot.docs.map(doc => {
-        console.log(doc.id);
-        console.log(doc.data());
         return { id: doc.id, ...doc.data() };
       })
     )
     .then(data => {
       res.json({ data });
     });
-  // db.ref("/catcalls").on("value", function(snapshot) {
-  //   console.log(snapshot.key);
-  // });
 };
 
 module.exports = {
