b58f3bbb1c684ac2db92551e3f73b7682531499e
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2019-03-14 19:48:29 -0400

    Updated back-end to give us item keys in response

--- src/routes/catcalls/catcalls.controller.js
+++ src/routes/catcalls/catcalls.controller.js
@@ -3,10 +3,19 @@ const db = require("../../db");
 const catcallsController = (req, res) => {
   db.collection("catcalls")
     .get()
-    .then(snapshot => snapshot.docs.map(doc => doc.data()))
+    .then(snapshot =>
+      snapshot.docs.map(doc => {
+        console.log(doc.id);
+        console.log(doc.data());
+        return { id: doc.id, ...doc.data() };
+      })
+    )
     .then(data => {
       res.json({ data });
     });
+  // db.ref("/catcalls").on("value", function(snapshot) {
+  //   console.log(snapshot.key);
+  // });
 };
 
 module.exports = {
